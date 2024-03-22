dba8b297bcad6a3f6dfc870681822b7eb0eeeecb
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-03-22 21:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let input = document.querySelector('#newTodoInput')
+let addBtn = document.querySelector('#addTodoBtn')
+let ol = document.querySelector('#todoList')
 
+addBtn.addEventListener("click",()=>{
+	if(input.value.trim() !== ""){
+		let li = document.createElement('li');
+		li.innerText = input.value;
+		ol.appendChild(li)
+		input.value = ""
+	}
+	
+})
